4d2ff3b1ab9c928ea03ef9cfc7bea90a8aa7676d
Budnikcam <138098381+Budnikcam@users.noreply.github.com>
2025-02-08 22:34:43 +0300

    Update user.py

--- backend/models/user.py
+++ backend/models/user.py
@@ -4,9 +4,14 @@ from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    username = db.Column(db.String(150), nullable=False, unique=True)
+    email = db.Column(db.String(150), nullable=False, unique=True)  # Убедитесь, что это поле есть
+    password = db.Column(db.String(150), nullable=False)
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
 
     def __repr__(self):
         return f'<User {self.username}>'
