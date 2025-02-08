7c3f4e9a4c884511fbd01d05310075a91eac0b10
Budnikcam <138098381+Budnikcam@users.noreply.github.com>
2025-02-09 00:21:21 +0300

    Update database.py

--- backend/models/database.py
+++ backend/models/database.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'qlite:///your_database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 'sqlite:///your_database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
