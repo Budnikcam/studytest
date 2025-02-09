84830dc9571af82d69a30b38d4b27fba0e699b00
Иван Соломичев <ivansolomicev@MacBook-Pro-Ivan.local>
2025-02-09 17:11:09 +0300

    Удаление всех файлов из репозитория

--- backend/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .database import db
-from .user import User
-from .lecture import Lecture
-from .practical_work import PracticalWork
-from .exam import Exam
-from .lecturesfile import LectureFile
